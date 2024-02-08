a18c3d83a603befdd7e494935fa491d53abf5084
AnkittChauhan <ankittrajput.4@gmail.com>
2024-02-08 16:00:08 +0530

    testing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-6xl font-bold text-yellow-400'>PokeMon</h1>
+      <h1 className='text-8xl font-bold text-yellow-400'>PokeMon Go Testing</h1>
     </div>
   )
 }
