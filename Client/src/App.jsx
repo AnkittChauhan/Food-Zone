28282381e9e4403c10e0c1ce08b1296a3fd7eadd
AnkittChauhan <ankittrajput.4@gmail.com>
2024-02-17 21:40:47 +0530

    registration

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,15 +3,17 @@ import Home from '../Components/Home'
 import LogInPage from '../Components/LogInPage'
 import PreLoginNavbar from '../Components/PreLoginNavbar'
 import LoggedinNavbar from '../Components/LoggedinNavbar'
+import SignUp from '../Components/SignUp'
 
 const App = () => {
   return (
     <div>
-       {/* < PreLoginNavbar/> */}
-    <LoggedinNavbar/>
-     < Home/>
+       < PreLoginNavbar/>
+    {/* <LoggedinNavbar/> */}
+     {/* < Home/> */}
      {/* < LogInPage/> */}
-    
+     < SignUp />
+
     </div>
   )
 }
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -9,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
      <NextUIProvider>
       <App />
     </NextUIProvider>
+    
   </React.StrictMode>,
 )
