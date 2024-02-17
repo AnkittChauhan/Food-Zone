b12e6952b58aee003734bd31ea19da306ca8604d
AnkittChauhan <ankittrajput.4@gmail.com>
2024-02-17 22:28:04 +0530

    changs

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,16 +4,17 @@ import LogInPage from '../Components/LogInPage'
 import PreLoginNavbar from '../Components/PreLoginNavbar'
 import LoggedinNavbar from '../Components/LoggedinNavbar'
 import SignUp from '../Components/SignUp'
+import FoodCards from '../Components/FoodCards'
 
 const App = () => {
   return (
     <div>
-       < PreLoginNavbar/>
-    {/* <LoggedinNavbar/> */}
+       {/* < PreLoginNavbar/> */}
+    <LoggedinNavbar/>
      {/* < Home/> */}
      {/* < LogInPage/> */}
-     < SignUp />
-
+     {/* < SignUp /> */}
+      < FoodCards />
     </div>
   )
 }
