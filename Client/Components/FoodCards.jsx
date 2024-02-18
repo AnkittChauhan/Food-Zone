0d92712594286fef3de076c339d25b29cf847864
AnkittChauhan <ankittrajput.4@gmail.com>
2024-02-18 13:11:09 +0530

    changes

--- Client/Components/FoodCards.jsx
+++ Client/Components/FoodCards.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 
 const FoodCards = () => {
 
-    const url = 'https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800';
+    const url = 'https://i.pinimg.com/736x/da/a4/fe/daa4feaa73c3376e3ebdcfc0531c0a2b.jpg';
     const name = "Pasta";
     const price = 100;
   return (
-    <li>
-    <div className='bg-gray-300 h-80 w-56 rounded-3xl shadow-xl'>
-        <img
-     className='rounded-t-3xl'
-     src={url}/>
-     <h1 className='text-2xl font-semibold tracking-wider'>Name : {name}</h1>
-     <h1 className=' font-normal tracking-wider'>Price : ₹{price}</h1>
-     <button className='bg-red-500 text-white h-8 w-2/3 rounded-md font-medium tracking-wider'>Delete Item</button>
-     </div>
-    </li>
+    <div className='mt-8 justify-items-center'>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-14 ml-6 gap-y-8'>
+
+            <li>
+              <div className='bg-gray-300 h-80 w-56 rounded-3xl shadow-xl'>
+                <div className='h-56'><img className='h-full w-full rounded-t-3xl object-fill' src={url} /></div>
+                <h1 className='text-2xl font-semibold tracking-wider text-center'>Name : {name}</h1>
+                <h1 className='font-normal tracking-wider text-center'>Price :₹{price}</h1>
+                <button className='bg-green-500 text-white h-8 w-2/3 rounded-md font-medium tracking-wider hover:bg-green-700 focus:transition-colors duration-200 active:scale-95 mx-9'>
+                  Add Item
+                </button>
+              </div>
+            </li>
+      
+        </ul>
+      </div>
 
       
 
--- Client/Components/LoggedinNavbar.jsx
+++ Client/Components/LoggedinNavbar.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
+
 
 const navigation = [
   { name: 'Home', href: '#', current: true },
@@ -65,7 +67,7 @@ export default function Example() {
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  <ShoppingBagIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
                 {/* Profile dropdown */}
