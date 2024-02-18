77d9ade93c160c32ac40f4456f67d77d0faac65a
AnkittChauhan <ankittrajput.4@gmail.com>
2024-02-18 13:39:31 +0530

    changes

--- Client/Components/LoggedinNavbar.jsx
+++ Client/Components/LoggedinNavbar.jsx
@@ -1,8 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { ShoppingBagIcon } from "@heroicons/react/24/solid";
-
+import {Badge} from "@nextui-org/react";
 
 const navigation = [
   { name: 'Home', href: '#', current: true },
@@ -67,7 +66,9 @@ export default function Example() {
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
-                  <ShoppingBagIcon className="h-6 w-6" aria-hidden="true" />
+                  <Badge content="0" className='bg-red-500 text-white' shape="circle" >
+                  <BellIcon className="h-4 w-6" aria-hidden="true" />
+                  </Badge>
                 </button>
 
                 {/* Profile dropdown */}
@@ -77,7 +78,7 @@ export default function Example() {
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
-                        className="h-8 w-8 rounded-full"
+                        className="h-9 w-9 rounded-full"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                       />
