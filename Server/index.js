adb56689e7728e07ebc71cbd5653f22a2f1e6866
AnkittChauhan <ankittrajput.4@gmail.com>
2024-02-25 21:39:04 +0530

    Server side

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = 8080;
+
+
+app.get('/home' , ( req , res ) => {
+
+
+})
+
+
+
+
+app.listen( PORT , () => {
+    console.log("Hurreyy !! Server is running");
+})
+
+
